a419b92fcbfe1449b17597e3761e126f0a4830d5
Matthew Munks <matthew.munks@exeter.ox.ac.uk>
2022-07-12 14:05:54 +0700

    Remoiving hard-coded poses. (Not localised poses).

--- state_machines/src/state_machines/robocup_2022/qualification_sm.py
+++ state_machines/src/state_machines/robocup_2022/qualification_sm.py
@@ -35,94 +35,6 @@ def construct_qualification_sm():
     end_loc = Pose();
     end_loc.orientation.w = 1;
     sm.userdata.pose = end_loc;
-    
-    """
-    Pose 1
-    header: 
-      seq: 12696
-      stamp: 
-        secs: 1657606062
-        nsecs: 462087244
-      frame_id: "map"
-    pose: 
-      position: 
-        x: 2.810693510548858
-        y: 0.07427706091467959
-        z: 0.0
-      orientation: 
-        x: 0.0
-        y: 0.0
-        z: 0.0264994773989117
-        w: 0.9996488271876202
-    """
-
-    look_at_pose = Pose();
-    look_at_pose.position.x = 2.810693510548858;
-    look_at_pose.position.y = 0.07427706091467959;
-    look_at_pose.position.z = 0.0;
-    look_at_pose.orientation.x = 0.0;
-    look_at_pose.orientation.y = 0.0;
-    look_at_pose.orientation.z = 0.0264994773989117;
-    look_at_pose.orientation.w = 0.9996488271876202;
-
-    """
-    Pose 2:
-    header: 
-    seq: 17671
-    stamp: 
-        secs: 1657606112
-        nsecs: 212031691
-    frame_id: "map"
-    pose: 
-    position: 
-        x: 2.7241360652618876
-        y: 2.4989383095654563
-        z: 0.0
-    orientation: 
-        x: 0.0
-        y: 0.0
-        z: 0.7055165951344321
-        w: 0.7086933991437467
-    """
-
-    intermediate_pose = Pose();
-    intermediate_pose.position.x = 2.7241360652618876
-    intermediate_pose.position.y = 2.4989383095654563
-    intermediate_pose.position.z = 0.0
-    intermediate_pose.orientation.x = 0.0;
-    intermediate_pose.orientation.y = 0.0;
-    intermediate_pose.orientation.z = 0.7055165951344321;
-    intermediate_pose.orientation.w = 0.7086933991437467;
-
-    """
-    Pose 3:
-    header: 
-    seq: 6730
-    stamp: 
-        secs: 1657606527
-        nsecs:  54727887
-    frame_id: "map"
-    pose: 
-    position: 
-        x: -0.00303225730938404
-        y: -0.002021541005732918
-        z: 0.0
-    orientation: 
-        x: 0.0
-        y: 0.0
-        z: 0.0041878305375389665
-        w: 0.9999912309992467
-    ---
-    """
-    final_pose = Pose();
-    final_pose.position.x = -0.00303225730938404;
-    final_pose.position.y = -0.002021541005732918;
-    final_pose.position.z =  0.0;
-    final_pose.orientation.x = 0.0
-    final_pose.orientation.y = 0.0
-    final_pose.orientation.z = 0.0041878305375389665
-    final_pose.orientation.w = 0.9999912309992467
-    sm.userdata.final_pose = final_pose;
 
     with sm:
 
@@ -148,15 +60,6 @@ def construct_qualification_sm():
                 'repeat_failure':'task_failure'},
             remapping={'node_id':'navigate_to_node'});
 
-        # smach.StateMachine.add(
-        #     'NavToOperator',
-        #     SimpleNavigateState(),
-        #     transitions={
-        #         'success':'WAIT_FOR_HOTWORD',
-        #         'failure':'NavToOperator',
-        #         }
-        # )
-
         smach.StateMachine.add(
             'WAIT_FOR_HOTWORD',
             WaitForHotwordState(),
@@ -174,14 +77,14 @@ def construct_qualification_sm():
                 'repeat_failure':'task_failure'},
             remapping={'node_id':'exit_node'});
 
-        smach.StateMachine.add(
-            'NAV_TO_START',
-            SimpleNavigateState(),
-            transitions={
-                'success':'task_success', 
-                'failure':'NAV_TO_START', 
-                'repeat_failure':'task_failure'},
-            remapping={'pose':'start_pose'});
+        # smach.StateMachine.add(
+        #     'NAV_TO_START',
+        #     SimpleNavigateState(),
+        #     transitions={
+        #         'success':'task_success', 
+        #         'failure':'NAV_TO_START', 
+        #         'repeat_failure':'task_failure'},
+        #     remapping={'pose':'start_pose'});
 
     return sm;
 
