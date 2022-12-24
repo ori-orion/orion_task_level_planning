05d5e0fa3485117f652551d56a3090c6b5362dab
Matthew Munks <matthewmunks@gmail.com>
2022-12-24 23:12:05 +0000

    Adding code for filtering out the unnecessary points.

--- state_machines/src/cpp/navigation.cpp
+++ state_machines/src/cpp/navigation.cpp
@@ -5,19 +5,56 @@ GettingSuitableNavGoal::GettingSuitableNavGoal(
     const Point_T& location_of_interest,
     const double& distance_away) 
     : location_of_interest(location_of_interest), distance_away(distance_away),
-    shared_cloud(new PointCloud()) {};
+    shared_cloud(new PointCloud()),
+    tf_buffer(), tf_listener(tf_buffer) {};
 GettingSuitableNavGoal::~GettingSuitableNavGoal() {};
-
-
-inline double GettingSuitableNavGoal::distance(const Point_T& p1, const Point_T& p2) {
+inline double sq_distance_2D(const Point_T& p1, const Point_T& p2) {
+    Point_T vec(p1.x-p2.x, p1.y-p2.y, 0);
+    return vec.x*vec.x + vec.y*vec.y;
+}
+inline double GettingSuitableNavGoal::sq_distance(const Point_T& p1, const Point_T& p2) {
     Point_T vec(p1.x-p2.x, p1.y-p2.y, p1.z-p2.z);
 
-    return std::sqrt(vec.x*vec.x + vec.y*vec.y + vec.z*vec.z);
+    return vec.x*vec.x + vec.y*vec.y + vec.z*vec.z;
 }
+inline double GettingSuitableNavGoal::distance(const Point_T& p1, const Point_T& p2) {
+    return std::sqrt(this->distance(p1, p2));
+}
+void GettingSuitableNavGoal::transformPointCloud(
+    const PointCloud& original, PointCloud& output) {
 
+
+    ros::Time time_message_received = ros::Time::now(); 
+    const std::string target_frame = "map";
+    const std::string source_frame = "head_rgbd_sensor_rgb_frame";
+
+    geometry_msgs::TransformStamped transform = this->tf_buffer.lookupTransform(
+        target_frame, source_frame, ros::Time(0));
+    Eigen::Isometry3d matrix_transform = tf2::transformToEigen(transform);
+    Eigen::Affine3d affine_transformation(matrix_transform);
+
+    pcl::transformPointCloud(original, output, affine_transformation);
+
+}
+void GettingSuitableNavGoal::filterOutFloor_FarObjs(pcl::PointIndices& output) {
+    // All vals are in mm
+    const double max_floor_height = 10;
+    const double max_height_of_interest = 2000;
+    const double max_radius_of_interest = 1000;
+    const double max_radius_of_interest_sq = max_radius_of_interest*max_radius_of_interest; 
+
+    for (int i = 0; i < this->shared_cloud->points.size(); i++) {
+        if (this->shared_cloud->points[i].z < max_floor_height)
+            continue;
+        if (this->shared_cloud->points[i].z > max_height_of_interest)
+            continue;
+        if (this->sq_distance_2D(this->shared_cloud->points[i], this->location_of_interest) > max_radius_of_interest_sq)
+            continue;
+
+        output.indices.push_back(i);
+    }
+}
 void GettingSuitableNavGoal::pointCloudCallback(const sensor_msgs::PointCloud2& msg) {
-
-    // mdo::Transformable transform_getter;
     
     ros::Time time_message_received = ros::Time::now(); 
     const std::string target = "map";
@@ -27,33 +64,27 @@ void GettingSuitableNavGoal::pointCloudCallback(const sensor_msgs::PointCloud2& 
     //     transform_getter.getTransform(target, source, ros::Time(0));
 
     // ROS_INFO("Data recieved: is_bigendian=", msg.is_bigendian, " data_size=", msg.data.size());
-    std::cout 
-        << "Data recieved: is_bigendian=" << msg.is_bigendian 
-        << " data_size=" << msg.data.size() 
-        << " (row_step, point_step)=(" << msg.row_step << ", " << msg.point_step << ")"
-        << " fields.size()=" << msg.fields.size()
-        << std::endl;
-    for(int i=0; i<msg.fields.size(); i++) {
-        std::cout 
-            << "\tname=" << msg.fields[i].name
-            << " \toffset=" << msg.fields[i].offset
-            << std::endl;
-    }
+    // std::cout 
+    //     << "Data recieved: is_bigendian=" << msg.is_bigendian 
+    //     << " data_size=" << msg.data.size() 
+    //     << " (row_step, point_step)=(" << msg.row_step << ", " << msg.point_step << ")"
+    //     << " fields.size()=" << msg.fields.size()
+    //     << std::endl;
+    // for(int i=0; i<msg.fields.size(); i++) {
+    //     std::cout 
+    //         << "\tname=" << msg.fields[i].name
+    //         << " \toffset=" << msg.fields[i].offset
+    //         << std::endl;
+    // }
 
-    // std::cout << "Pre boost creation" << std::endl;
-    // boost::shared_ptr<mdo::PointCloud> cloud;
-    // std::cout << "Pre fromROSMsg function call" << std::endl;
-    // pcl::fromROSMsg(msg, *cloud);
-
-    std::cout << "Creating shared_cloud" << std::endl;
-    // pcl::shared_ptr<PointCloud> shared_cloud(new PointCloud());
-
-    std::cout << "Loading point cloud." << std::endl;
-    PointCloud& cloud = *(this->shared_cloud.get());
+    PointCloud cloud;
     pcl::fromROSMsg(msg, cloud);
+    this->transformPointCloud(cloud, *(this->shared_cloud.get()));
 
-    std::cout << "Received cloud with " << cloud.points.size() << " points.";
+    std::cout << "Received cloud with " << this->shared_cloud->points.size() << " points.";
     
+    pcl::PointIndices indices;
+    this->filterOutFloor_FarObjs(indices);
     // pcl::io::savePLYFileBinary("./point_cloud", cloud);
 
     // std::cout << current_transform << std::endl;
@@ -67,12 +98,10 @@ void GettingSuitableNavGoal::pointCloudCallback(const sensor_msgs::PointCloud2& 
 
     ros::shutdown();
 }
-
-
 pcl::search::Search<Point_T>::Ptr GettingSuitableNavGoal::getSearchTree(){
     if (this->search_tree.get() == nullptr) {
-        this->search_tree = boost::shared_ptr<pcl::search::KdTree<mdo::Point_T>>(
-            new pcl::search::KdTree<mdo::Point_T>());
+        this->search_tree = boost::shared_ptr<pcl::search::KdTree<Point_T>>(
+            new pcl::search::KdTree<Point_T>());
         
         this->search_tree->setInputCloud(this->shared_cloud);
     }
@@ -88,7 +117,7 @@ int main(int argc, char **argv) {
     // ros::NodeHandle n;
 
     // std::cout << "Waiting 2 secs" << std::endl;
-    // mdo::Transformable transform_getter;
+    // Transformable transform_getter;
     // ros::Time::sleepUntil(ros::Time::now() + ros::Duration(2));
     // std::cout << "Finished waiting" << std::endl;
 
--- state_machines/src/cpp/navigation.h
+++ state_machines/src/cpp/navigation.h
@@ -1,20 +1,25 @@
 #include <iostream>
 
 // #include <eigen_conversions/eigen_msg.h>
-#include <pcl_conversions/pcl_conversions.h>
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_eigen/tf2_eigen.h> 
 
+#include <pcl/common/transforms.h>
 #include <pcl/point_cloud.h>
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/io/ply_io.h>
 #include <pcl/search/kdtree.h>
-// #include <pcl/conversions.h>
+#include <pcl/PointIndices.h>
+#include <pcl_conversions/pcl_conversions.h>
 
 #include <memory>
 
-#include <math>
+
+// #include <math>
 
 using Point_T = pcl::PointXYZRGB;
 using PointCloud = pcl::PointCloud<Point_T>;
@@ -42,6 +47,9 @@ private:
 
     pcl::shared_ptr<PointCloud> shared_cloud;
 
+    tf2_ros::Buffer tf_buffer;
+    tf2_ros::TransformListener tf_listener;
+
 
 public:
     GettingSuitableNavGoal(
@@ -53,8 +61,28 @@ private:
     /*
     Gets the distance between two points.
     */
+    inline double sq_distance(const Point_T& p1, const Point_T& p2);
     inline double distance(const Point_T& p1, const Point_T& p2);
 
+    /*
+    Returns the squared distance between two points, but does so only in the plane. 
+
+    For navigation, we're only really interested in that bit anyway.
+    */
+    inline double sq_distance_2D(const Point_T& p1, const Point_T& p2);
+
+
+    /*
+    Transforms the point cloud to the map frame.
+    */
+    void transformPointCloud(const PointCloud& original, PointCloud& output);
+
+    /*
+    Removes all the points at floor level.
+    */
+    void filterOutFloor_FarObjs(pcl::PointIndices& output);
+
+    
     /*
     The callback for getting a point cloud as input.
 
@@ -68,7 +96,7 @@ private:
     This function creates the search tree if necessary. It then returns the search tree.
     */
     pcl::search::Search<Point_T>::Ptr getSearchTree();
-}
+};
 
 
 /*
