d47cbf9de1cacaea89eed60accebbe466dfd1271
Matthew Munks <matthewmunks@gmail.com>
2022-12-24 18:20:21 +0000

    Looking at setting up the kd tree.

--- state_machines/src/cpp/navigation.cpp
+++ state_machines/src/cpp/navigation.cpp
@@ -1,13 +1,21 @@
 #include "navigation.h"
 
 
-inline double distance(const Point_T& p1, const Point_T& p2) {
+GettingSuitableNavGoal::GettingSuitableNavGoal(
+    const Point_T& location_of_interest,
+    const double& distance_away) 
+    : location_of_interest(location_of_interest), distance_away(distance_away),
+    shared_cloud(new PointCloud()) {};
+GettingSuitableNavGoal::~GettingSuitableNavGoal() {};
+
+
+inline double GettingSuitableNavGoal::distance(const Point_T& p1, const Point_T& p2) {
     Point_T vec(p1.x-p2.x, p1.y-p2.y, p1.z-p2.z);
 
     return std::sqrt(vec.x*vec.x + vec.y*vec.y + vec.z*vec.z);
 }
 
-void pointCloudCallback(const sensor_msgs::PointCloud2& msg) {
+void GettingSuitableNavGoal::pointCloudCallback(const sensor_msgs::PointCloud2& msg) {
 
     // mdo::Transformable transform_getter;
     
@@ -38,10 +46,10 @@ void pointCloudCallback(const sensor_msgs::PointCloud2& msg) {
     // pcl::fromROSMsg(msg, *cloud);
 
     std::cout << "Creating shared_cloud" << std::endl;
-    pcl::shared_ptr<PointCloud> shared_cloud(new PointCloud());
+    // pcl::shared_ptr<PointCloud> shared_cloud(new PointCloud());
 
     std::cout << "Loading point cloud." << std::endl;
-    PointCloud& cloud = *(shared_cloud.get());
+    PointCloud& cloud = *(this->shared_cloud.get());
     pcl::fromROSMsg(msg, cloud);
 
     std::cout << "Received cloud with " << cloud.points.size() << " points.";
@@ -52,13 +60,25 @@ void pointCloudCallback(const sensor_msgs::PointCloud2& msg) {
 
     //... populate cloud
     pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
-    viewer.showCloud(shared_cloud);
+    viewer.showCloud(this->shared_cloud);
     while (!viewer.wasStopped ())
     {
     }
 
     ros::shutdown();
 }
+
+
+pcl::search::Search<Point_T>::Ptr GettingSuitableNavGoal::getSearchTree(){
+    if (this->search_tree.get() == nullptr) {
+        this->search_tree = boost::shared_ptr<pcl::search::KdTree<mdo::Point_T>>(
+            new pcl::search::KdTree<mdo::Point_T>());
+        
+        this->search_tree->setInputCloud(this->shared_cloud);
+    }
+    return this->search_tree;
+}
+
 
 int main(int argc, char **argv) {
     // ROS_DEBUG("Hello world");
--- state_machines/src/cpp/navigation.h
+++ state_machines/src/cpp/navigation.h
@@ -9,7 +9,7 @@
 #include <pcl/point_cloud.h>
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/io/ply_io.h>
-
+#include <pcl/search/kdtree.h>
 // #include <pcl/conversions.h>
 
 #include <memory>
@@ -20,17 +20,55 @@ using Point_T = pcl::PointXYZRGB;
 using PointCloud = pcl::PointCloud<Point_T>;
 
 
-/*
-Gets the distance between two points.
-*/
-inline double distance(const Point_T& p1, const Point_T& p2);
 
 /*
-The callback for getting a point cloud as input.
-
-Is the callback for a message of type point cloud.
+There are multiple situations where we have something we might want
+to pick up, or put down in a certain location. This involves 
+navigating to a place NEARBY BUT NOT ONTOP OF a certain goal 
+location. This class will work out the suitable navigation goal 
+involved.
 */
-void pointCloudCallback(const sensor_msgs::PointCloud2& msg);
+class GettingSuitableNavGoal {
+private:
+    // The point we want to get close to.
+    Point_T location_of_interest;
+
+    // The distance away from the object we want to end up in mm.
+    double distance_away;
+
+    
+    // The search tree for finding the closest points.
+    pcl::search::Search<Point_T>::Ptr search_tree;
+
+    pcl::shared_ptr<PointCloud> shared_cloud;
+
+
+public:
+    GettingSuitableNavGoal(
+        const Point_T& location_of_interest,
+        const double& distance_away=500);
+    ~GettingSuitableNavGoal();
+
+private:
+    /*
+    Gets the distance between two points.
+    */
+    inline double distance(const Point_T& p1, const Point_T& p2);
+
+    /*
+    The callback for getting a point cloud as input.
+
+    Is the callback for a message of type point cloud.
+    */
+    void pointCloudCallback(const sensor_msgs::PointCloud2& msg);
+
+    
+private:
+    /*
+    This function creates the search tree if necessary. It then returns the search tree.
+    */
+    pcl::search::Search<Point_T>::Ptr getSearchTree();
+}
 
 
 /*
