e0b539a55059a271e0f688079a6eebb1405e8a4c
MatthewMunks <matthewmunks@gmail.com>
2023-04-27 15:20:43 +0100

    Moving the find placement test script into the manipulation tests.

--- state_machines/src/state_machines/tests/test_manipulation.py
+++ state_machines/src/state_machines/tests/test_manipulation.py
@@ -12,7 +12,14 @@ import smach_ros
 import actionlib
 
 from state_machines.Reusable_States.include_all import *;
+from state_machines.Reusable_States.training import *;
 
+# from state_machines.SubStateMachines.create_sub_state_machines import *;
+
+# from state_machines.reusable_states import * # pylint: disable=unused-wildcard-import
+# from set_up_clients import create_stage_1_clients
+from orion_actions.msg import SOMObservation, Relation, SearchPersonNotMetGoal
+from geometry_msgs.msg import Pose
 # from state_machines.reusable_states import * # pylint: disable=unused-wildcard-import
 
 
@@ -103,11 +110,87 @@ def create_state_machine(userdata=None):
     return sm
 
 
-if __name__ == '__main__':
-    rospy.init_node('speech_test')
+def test_finding_placement():
+    """ 
+    This is a test script for working out where to place an object if you
+    want to place it next to an object.
+
+    At present, the options should appear in RViz as TFs.
+    TODO:
+        Get placement options through.
+        Place an object at one of these target locations. 
+    """
 
     # Create the state machine
-    sm = create_state_machine()
+    sm = smach.StateMachine(outcomes=[TASK_SUCCESS, TASK_FAILURE]);
+
+    # Wait for the prameters to be loaded.
+    # while (not rospy.has_param('params_loaded')):
+    #     rospy.sleep(0.5);
+
+    # Create state machine userdata dictionary elements
+
+    # Task params
+    sm.userdata.obj_class = 'bottle'
+
+    dims = (0.05, 0.05, 0.2) 
+    height = 1
+    radius = 0.2
+
+
+    with sm:
+
+        smach.StateMachine.add(
+            'CREATE_OBJECT_QUERY',
+            CreateSOMQuery(
+                CreateSOMQuery.OBJECT_QUERY, 
+                save_time=False),
+            transitions={
+                SUCCESS: 'FIND_OBJECT'},
+            remapping={
+                'class_': 'obj_class'
+            })
+        
+        smach.StateMachine.add(
+            'FIND_OBJECT',
+            PerformSOMQuery(),
+            transitions={
+                SUCCESS:'FIND_PLACEMENT',
+                FAILURE:TASK_FAILURE},
+            remapping={})
+
+        smach.StateMachine.add(
+            'FIND_PLACEMENT',
+            PlaceNextTo(dims, height, radius),
+            transitions={
+                SUCCESS: TASK_SUCCESS,
+                FAILURE: 'PRINT_FAILURE'
+            },
+            remapping={}
+        )
+
+        smach.StateMachine.add(
+            'PRINT_FAILURE',
+            PrintToConsole(phrase="Failed to find placement"),
+            transitions = {
+                SUCCESS: TASK_FAILURE,
+                FAILURE: TASK_FAILURE
+            }
+        )
+        
+        sm = setupErrorStates(sm)
+        
+        # TODO - Reset e
+    return sm
+    
+
+
+if __name__ == '__main__':
+    rospy.init_node('manipulation_test')
+
+    # Create the state machine
+    # sm = create_state_machine()
+    sm = test_finding_placement();
 
     # Create and start the introspection server
     sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
